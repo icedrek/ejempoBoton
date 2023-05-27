a3fa7463e2166e8345499660bf8fd42269a44726
icedrek <icedrek@gmail.com>
2023-05-27 07:36:18 +0200

    first example

--- app/src/main/java/com/example/ejemploboton/MainActivity.kt
+++ app/src/main/java/com/example/ejemploboton/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.ejemploboton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val newText: String = this.getString(R.string.greeting2)
+        findViewById<Button>(R.id.button)
+            .setOnClickListener {
+                findViewById<TextView>(R.id.textView).text = newText
+            }
     }
 }
